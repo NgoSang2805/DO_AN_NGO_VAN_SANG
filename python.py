cb28dd0d35063dc176659d1d0c459989183dd2ee
NgoSang2805 <ngosang2805@gmail.com>
2024-11-12 23:39:01 +0700

    fix chan doan

--- python.py
+++ python.py
@@ -11,7 +11,7 @@ white_blood_cells = np.random.randint(5000, 15000, num_patients)
 blood_glucose_levels = np.round(np.random.uniform(4.5, 8.0, num_patients), 1)
 test_results = np.random.choice(['Bình thường', 'Bất thường'], num_patients)
 symptoms_list = ['Mệt mỏi', 'Giảm cân', 'Đau ngực', 'Khó thở', 'Ho kéo dài', 'Sốt', 'Chán ăn']
-diagnoses = np.random.choice([0, 1], num_patients)
+diagnoses = np.random.choice([0, 1, 2], num_patients)
 
 # Hàm tạo triệu chứng ngẫu nhiên
 def generate_symptoms(symptoms_list):
@@ -31,7 +31,7 @@ data = {
     'Mức độ đường huyết': blood_glucose_levels,
     'Kết quả xét nghiệm': test_results,
     'Triệu chứng': symptoms,
-    'Chẩn đoán bệnh (0: Không, 1: Có)': diagnoses
+    'Chẩn đoán bệnh (0: Không, 1: Có, 2: Bệnh khác)': diagnoses
 }
 
 df = pd.DataFrame(data)
