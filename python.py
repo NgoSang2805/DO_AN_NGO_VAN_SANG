0ab5daa9e3ff114d86edbfd86fc8f4b38779c79f
NgoSang2805 <ngosang2805@gmail.com>
2024-11-27 01:58:40 +0700

    27-11

--- python.py
+++ python.py
@@ -61,3 +61,46 @@ df = pd.DataFrame(data, columns=['Breed', 'Color', 'Height', 'Length', 'Weight',
 df.to_csv('cats_dataset.csv', index=False, encoding='utf-8-sig')
 
 print(df.head())  # Hiển thị 5 hàng đầu tiên của dataset
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Vẽ biểu đồ cột thể hiện số lượng của từng giống mèo
+plt.figure(figsize=(10, 6))
+sns.countplot(x='Breed', data=df, palette='Set3')
+plt.title('Số lượng của từng giống mèo')
+plt.xlabel('Giống mèo')
+plt.ylabel('Số lượng')
+plt.show()
+
+#Training dữ liệu
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, accuracy_score
+
+# Đọc dữ liệu từ file CSV
+df = pd.read_csv('cats_dataset.csv')
+
+# Mã hóa biến phân loại
+label_encoder = LabelEncoder()
+df['Color'] = label_encoder.fit_transform(df['Color'])
+
+# Xáo trộn và chia dữ liệu thành tập huấn luyện và tập kiểm tra
+X = df[['Color', 'Height', 'Length', 'Weight', 'Age']]
+y = df['Breed']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=20/200, random_state=42, stratify=y, shuffle=True)
+
+# Khởi tạo mô hình K-NN
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# Huấn luyện mô hình
+knn.fit(X_train, y_train)
+
+# Dự đoán trên tập kiểm tra
+y_pred = knn.predict(X_test)
+
+# Đánh giá mô hình
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
